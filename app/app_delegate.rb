47e7e3c1a1557e6520b01aa58c14cb6c59fb4614
Javad Afshar <mjafshar@gmail.com>
2014-04-15 18:51:16 -0500

    Created alphabet_controller and app_delegate.

---
+++ app/app_delegate.rb
@@ -0,0 +1,10 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle:nil)
+    @window.rootViewController =
+      UINavigationController.alloc.initWithrootViewController(@alphabet_controller)
+    @window.makeKeyAndVisible
+    true
+  end
+end
---
+++ app/controllers/AlphabetController.rb
@@ -0,0 +1,9 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+    self.title = "Alphabet"
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
+    self.view.addSubview(@table)
+  end
+end
